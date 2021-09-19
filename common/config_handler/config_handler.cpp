0d8f050689dddfd1a8d4c3a8f25535060f92cb2a
ziqiu zhou <zzq1063748118@gmail.com>
2021-09-19 12:31:56 +0200

    feat: add config file handler

---
+++ common/config_handler/config_handler.cpp
@@ -0,0 +1,140 @@
+#pragma warning(disable : 4996)
+
+#include "config_handler.h"
+
+namespace common::config_handler {
+	const std::size_t BUF_SIZE = 501;
+
+	ConfigFileHandler* ConfigFileHandler::m_instance = nullptr;
+
+	std::unordered_map<std::string, std::string> ConfigFileHandler::config_items;
+
+	ConfigFileHandler::~ConfigFileHandler() {
+		config_items.clear();
+	}
+
+	// check a POSIX error code
+	static void posixCheck(int errorCode) {
+		if (errorCode != 0) {
+			throw std::system_error(std::error_code(errorCode, std::generic_category()));
+		}
+	}
+
+	// check the status code of functions that return an error code in `errno`.
+	static void posixAssert(bool success) {
+		if (!success) {
+			posixCheck(errno);
+		}
+	}
+
+	// Truncate the leading and trailing spaces of the string
+	void Trim(std::string& str) {
+		if (!str.empty())
+		{
+			str.erase(0, str.find_first_not_of(" "));
+			str.erase(str.find_last_not_of(" ") + 1);
+		}
+		return;
+	}
+
+	ConfigFileHandler::FileHandle ConfigFileHandler::FileReader::openFile(const std::filesystem::path& config_file) {
+		std::FILE* file = fopen(config_file.string().c_str(), "r");
+		posixAssert(file != nullptr);
+		return FileHandle(file, FileDeleter());
+	}
+
+	void ConfigFileHandler::FileReader::closeFile(FileHandle& file) {
+		std::FILE* rawfile = file.release();
+
+		// Close the file, throw an exception on error.
+		int result = fclose(rawfile);
+		posixAssert(result == 0);
+	}
+
+	bool ConfigFileHandler::FileReader::readFile(char* linebuf) {
+		//检查文件是否结束 ，没有结束则条件成立
+
+		while (!std::feof(file_.get())) {
+			if (fgets(linebuf, 500, file_.get()) == nullptr) { //从文件中读数据，每次读一行，一行最多不要超过500个字符
+				continue;
+			}
+			else if (linebuf[0] == 0) {
+				continue;
+			}
+			else if (linebuf[0] == ';' || linebuf[0] == ' ' || linebuf[0] == '#' || linebuf[0] == '\t' || linebuf[0] == '\n') { // 注释行
+				continue;
+			}
+
+		lblprocstring:
+			//后边若有换行，回车，空格等都截取掉
+			if (strlen(linebuf) > 0)
+			{
+				int length = strlen(linebuf) - 1;
+				if (linebuf[length] == 10 || linebuf[length] == 13 || linebuf[length] == 32)
+				{
+					linebuf[length] = 0;
+					goto lblprocstring;
+				}
+			}
+			if (linebuf[0] == 0) {
+				continue;
+			}
+			if (linebuf[0] == '[') { //[开头的也不处理
+				continue;
+			}
+			std::string line = linebuf;
+			auto pos = line.find('=');
+			if (pos < line.size() - 1 && pos != std::string::npos) {
+				std::string config_name(pos, '\0');
+				std::copy(line.begin(), line.begin() + pos, config_name.begin());
+
+				std::string config_content(line.size() - pos - 1, '\0');
+				std::copy(line.begin() + pos + 1, line.end(), config_content.begin());
+
+				// 截取字符串空格, included in ngx_string.cxx
+				Trim(config_name);
+				Trim(config_content);
+
+				ConfigFileHandler::config_items[config_name] = config_content;
+			}
+			line.clear();
+		}
+		return true;
+	}
+
+	bool ConfigFileHandler::Load(const std::filesystem::path& config_file) {
+		// open file
+		auto file_reader = FileReader::open(config_file.string().c_str());
+		char linebuf[BUF_SIZE];
+
+		bool result = file_reader->readFile(linebuf);
+		posixAssert(result);
+        
+		return true;
+	}
+
+	const std::string ConfigFileHandler::GetValue(const std::string& config_name) {
+		auto config_pos = config_items.find(config_name);
+		if (config_pos != config_items.end()) {
+			return config_items[config_name];
+		}
+		return std::string();
+	}
+
+	const int ConfigFileHandler::GetValue(const std::string& config_name, const int default_value) {
+		auto config_pos = config_items.find(config_name);
+		if (config_pos != config_items.end()) {
+			return std::stoi(config_items[config_name]);
+		}
+		return default_value;
+	}
+
+	const double ConfigFileHandler::GetValue(const std::string& config_name, const double default_value) {
+		auto config_pos = config_items.find(config_name);
+		if (config_pos != config_items.end()) {
+			return std::stod(config_items[config_name]);
+		}
+		return default_value;
+	}
+}
+
---
+++ common/config_handler/config_handler.h
@@ -0,0 +1,81 @@
+#pragma once
+
+#include <unordered_map>
+#include <string>
+#include <filesystem>
+
+namespace common::config_handler {
+	class ConfigFileHandler {
+	private:
+		ConfigFileHandler() {}
+
+		static ConfigFileHandler* m_instance;
+
+	public:
+		~ConfigFileHandler();
+
+	public:
+		static ConfigFileHandler* GetInstance() {
+			if (m_instance == nullptr) {
+				//锁（本来该加锁，但在主线程中率先执行了GetInstance函数，这样就不存在多线程调用该函数导致的需要互斥的问题，因此这里就没有实际加锁）
+				if (m_instance == nullptr) {
+					m_instance = new ConfigFileHandler();
+				}
+				//放锁
+			}
+			return m_instance;
+		}
+
+	public:
+
+		struct FileDeleter {
+			void operator ()(std::FILE* file) const noexcept {
+				// This function is called by the destructor of `std::unique_ptr<>`, so we ignore any errors returned by
+				// `std::fclose()`.
+				std::fclose(file);
+			}
+		};
+
+		using FileHandle = std::unique_ptr<std::FILE, FileDeleter>;
+
+		class FileReader {
+		private:
+			// Define a "file handle" as a `unique_ptr<>` with a custom deleter: lifetime management is done as usual, but when
+			// the resource is to be freed, `unique_ptr<>` will pass the file pointer to an instance of our deleter rather than
+			// using a `delete` expression.
+			FileHandle file_;
+
+		private:
+			FileHandle openFile(const std::filesystem::path& config_file);
+
+			void closeFile(FileHandle& file);
+
+			FileReader(const std::filesystem::path& config_file)
+				: file_(std::move(openFile(config_file))) {}
+
+		public:
+			static FileReader* open(const std::filesystem::path& config_file) {
+				static FileReader instance(config_file);
+				return &instance;
+			}
+
+			bool readFile(char* buffer);
+
+			~FileReader() {
+				closeFile(file_);
+			}
+		};
+
+	public:
+		bool Load(const std::filesystem::path& config_file); // load config file
+		
+		const std::string GetValue(const std::string& config_name);
+		
+		const int GetValue(const std::string& config_name, const int default_value);
+
+		const double GetValue(const std::string& config_name, const double default_value);
+
+	public:
+		static std::unordered_map<std::string, std::string> config_items;
+	};
+}
